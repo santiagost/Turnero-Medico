c8345cf71b41e651fab10e1bc007381363912cba
Juan Cruz Ceballos <94239@sistemas.frc.utn.edu.ar>
2025-11-08 20:42:19 -0300

    [ADD] [FRONTEND] Boton para resetear formulario de filtros

--- frontend/src/components/features/filterPanel/ConsultationFilterPanel.jsx
+++ frontend/src/components/features/filterPanel/ConsultationFilterPanel.jsx
@@ -2,18 +2,22 @@ import React, { useState } from 'react';
 import Input from '../../ui/Input';
 import Select from '../../ui/Select';
 import Button from '../../ui/Button';
+import IconButton from '../../ui/IconButton'
+import { motion } from 'framer-motion';
 
 import { FaSearch } from "react-icons/fa";
+import { LiaUndoAltSolid } from "react-icons/lia";
 
+const initialFiltersState = {
+    specialty: "",
+    doctor: "",
+    date: "",
+    order: "date_desc"
+};
 
 
 const ConsultationFilterPanel = ({ onSearch, specialties = [], doctors = [] }) => {
-    const [localFilters, setLocalFilters] = useState({
-        specialty: "",
-        doctor: "",
-        date: "",
-        order: "date_desc"
-    });
+    const [localFilters, setLocalFilters] = useState(initialFiltersState);
 
     const orderOptions = [
         { value: "date_desc", label: "Más reciente" },
@@ -43,6 +47,11 @@ const ConsultationFilterPanel = ({ onSearch, specialties = [], doctors = [] }) =
     const handleSearchClick = (e) => {
         e.preventDefault();
         onSearch(localFilters);
+    };
+
+    const handleResetClick = () => {
+        setLocalFilters(initialFiltersState);
+        onSearch(initialFiltersState);
     };
 
     return (
@@ -86,8 +95,17 @@ const ConsultationFilterPanel = ({ onSearch, specialties = [], doctors = [] }) =
                 size="small"
             />
 
-            <div className='flex flex-col items-center justify-center h-full'>
+            <div className='flex flex-row items-center justify-center h-full gap-5 text-white'>
                 <Button text={"Buscar"} icon={<FaSearch />} variant={"primary"} type={"submit"} size={"big"} />
+                <motion.div
+                    whileTap={{ scale: 0.9, rotate: -180 }}
+                >
+                    <IconButton
+                        icon={<LiaUndoAltSolid size={30} />}
+                        type={"button"}
+                        onClick={handleResetClick}
+                    />
+                </motion.div>
             </div>
         </form>
     );
