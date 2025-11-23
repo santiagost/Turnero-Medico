fe38e9300d35bc4ab6033e7c21677dd513af504e
FacuWitt <facu.witt@gmail.com>
2025-11-22 21:57:24 -0300

    [FIX] usuario y medico agregados en el __init__.py de routers

--- backend/api.py
+++ backend/api.py
@@ -45,9 +45,8 @@ from database import DatabaseConnection
 DatabaseConnection()
 
 # Registrar los routers de los controladores
-from routers import path_router, paciente_router, rol_router, especialidad_router, obra_social_router, estado_turno_router
-from routers.usuario_router import router as usuario_router  # no se por que no me deja importar como los de arriba
-from routers.medico_router import router as medico_router
+from routers import path_router, paciente_router, rol_router, especialidad_router, obra_social_router, estado_turno_router, usuario_router, medico_router
+
 
 FastAPIApp.include_router(path_router)
 FastAPIApp.include_router(paciente_router)
